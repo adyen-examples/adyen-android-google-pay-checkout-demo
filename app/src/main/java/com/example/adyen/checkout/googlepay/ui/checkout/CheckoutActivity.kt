ec9371a9a528a8513370e420f370d030f9361c1c
josephj <joseph.jreij@adyen.com>
2025-04-23 11:05:29 +0200

    Handle incoming redirects from 3DS

--- app/src/main/java/com/example/adyen/checkout/googlepay/ui/checkout/CheckoutActivity.kt
+++ app/src/main/java/com/example/adyen/checkout/googlepay/ui/checkout/CheckoutActivity.kt
@@ -1,9 +1,11 @@
 package com.example.adyen.checkout.googlepay.ui.checkout
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import com.adyen.checkout.redirect.RedirectComponent
 
 class CheckoutActivity : AppCompatActivity() {
 
@@ -15,6 +17,15 @@ class CheckoutActivity : AppCompatActivity() {
             CheckoutScreen(
                 viewModel = checkoutViewModel,
             )
+        }
+    }
+
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+
+        val data = intent.data
+        if (data != null && data.toString().startsWith(RedirectComponent.REDIRECT_RESULT_SCHEME)) {
+            checkoutViewModel.handleIntent(intent)
         }
     }
 }
